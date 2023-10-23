84ca8e13ed742ba9d9578d9db861f96dbd8d00f7
thiennbao <thiennbao.204@gmail.com>
2023-10-23 14:32:18 +0700

    add single course page

--- client/src/pages/site/CoursesPage/SingleCoursePage/index.jsx
+++ client/src/pages/site/CoursesPage/SingleCoursePage/index.jsx
@@ -1,13 +1,13 @@
 import { courseAPI } from "api";
-import Appearance from "components/Appearance";
-import Heading from "components/Heading";
-import Wallpaper from "components/Wallpaper";
-import SiteLayout from "layouts/SiteLayout";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useLocation } from "react-router-dom";
 import style from "./SingleCoursePage.module.scss";
-import FooterImages from "components/FooterImages";
+import Appearance from "components/Appearance";
+import Heading from "components/Heading";
+import Wallpaper from "components/Wallpaper";
+import SiteLayout from "layouts/SiteLayout";
+import { TypingFeild } from "components/Editor";
 
 const SingleCoursePage = () => {
   const id = useLocation().pathname.split("/")[2];
@@ -34,94 +34,104 @@ const SingleCoursePage = () => {
 
   return (
     <SiteLayout>
-      <Wallpaper page={course.name} image={course.thumbnail} />
-      <section className="container">
-        <div className="row justify-content-center">
-          <div className="col-6">
-            <Heading className="fs-2 text-center" subcontent="ABOUT">
-              {course.name}
-            </Heading>
-            <p className="my-4">{course.description}</p>
-            <p className="d-flex justify-content-around">
-              <span>Age: {course.age}</span>
-              <span>Lesson: {course.lesson}</span>
-              <span>Time: {course.time}h / lesson</span>
-            </p>
-          </div>
-        </div>
-        <div className="container">
-          <div className="row g-1">
-            {course.images && course.images.map((image, index) => (
-              <div className="col-4">
-                <img key={index} src={image} alt="" className={style.image} />
+      {course.name ? (
+        <>
+          <Wallpaper page={course.name} image={course.thumbnail} />
+          <section className={style.courseDetail}>
+            <div className="container">
+              <div className="row gy-4 justify-content-between align-items-stretch">
+                <div className="col-lg-5 col-md-6">
+                  <Appearance type="right">
+                    <Heading className="fs-2" subcontent="ABOUT">
+                      {course.name}
+                    </Heading>
+                    <p>{course.description}</p>
+                    <p>
+                      <i class="bi bi-person-circle"></i> Age: {course.age}
+                    </p>
+                    <p>
+                      <i class="bi bi-journals"></i> Lesson: {course.lesson}
+                    </p>
+                    <p>
+                      <i class="bi bi-clock-fill"></i> Time: {course.time}m / lesson
+                    </p>
+                  </Appearance>
+                </div>
+                <div className="col-md-6 d-flex">
+                  <div className="row gy-lg-0 gy-3">
+                    <div className="col-lg-6 col-md-12 col-sm-6 col-12 d-flex">
+                      <img src={course.images[0]} alt="" />
+                    </div>
+                    <div className="col-lg-6 col-md-12 col-sm-6 col-12 d-flex">
+                      <img src={course.images[1]} alt="" />
+                    </div>
+                  </div>
+                </div>
               </div>
-            ))}
-          </div>
-        </div>
-      </section>
-      <section className="container">
-        <div className={style.form}>
-          <Appearance type="up">
-            <Heading className="text-center fs-2">Register this course</Heading>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="row g-3">
-                <div className="col-md-6">
-                  <label>Name</label>
-                  <input
+            </div>
+          </section>
+          <section className="container">
+            <Appearance type="up">
+              <div className={style.form}>
+                <Heading className="text-center fs-2">Register this course</Heading>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <TypingFeild
+                    label="Name"
                     placeholder="Name"
-                    {...register("name", { required: true })}
+                    register={register("name", { required: true })}
                     aria-invalid={!!errors.name}
                     onFocus={() => clearErrors("name")}
                   />
-                  {errors.name && <span>Please fill out this field</span>}
-                </div>
-                <div className="col-md-6">
-                  <label>Phone</label>
-                  <input
+                  <TypingFeild
+                    label="Phone"
                     placeholder="Phone"
-                    {...register("phone", { required: true })}
+                    register={register("phone", { required: true })}
                     aria-invalid={!!errors.phone}
                     onFocus={() => clearErrors("phone")}
                   />
-                  {errors.phone && <span>Please fill out this field</span>}
-                </div>
-                <div>
-                  <label>Email</label>
-                  <input
+                  <TypingFeild
+                    label="Email"
                     placeholder="Email"
-                    {...register("email", { required: true })}
+                    type="email"
+                    register={register("email", { required: true })}
                     aria-invalid={!!errors.email}
                     onFocus={() => clearErrors("email")}
                   />
-                  {errors.email && <span>Please fill out this field</span>}
-                </div>
-                <div>
-                  <label>Subject</label>
-                  <input
-                    placeholder="Subject"
-                    {...register("subject", { required: true })}
-                    aria-invalid={!!errors.subject}
-                    onFocus={() => clearErrors("subject")}
-                  />
-                  {errors.subject && <span>Please fill out this field</span>}
-                </div>
-                <div>
-                  <label>Message</label>
-                  <textarea
+                  <TypingFeild
+                    label="Message"
+                    textarea
                     placeholder="Message"
-                    {...register("message", { required: true })}
+                    register={register("message", { required: true })}
                     aria-invalid={!!errors.message}
                     onFocus={() => clearErrors("message")}
                   />
-                  {errors.message && <span>Please fill out this field</span>}
-                </div>
-                <input type="submit" value="SEND MESSAGE" />
+                  <div className="mt-3">
+                    <input type="submit" value="SEND MESSAGE" />
+                  </div>
+                </form>
               </div>
-            </form>
-          </Appearance>
-        </div>
-      </section>
-      <FooterImages />
+            </Appearance>
+          </section>
+          <section className={style.images}>
+            <div className="container-fluid">
+              <div className="row g-1">
+                {course.images.slice(2).map((image) => (
+                  <div key={image} className="col-sm-6 col-md-4">
+                    <img src={image} alt="" />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
+        </>
+      ) : (
+        <section className={style.loading}>
+          <h3>
+            <span>Loading</span>
+            <span className="spinner-border mx-3" role="status"></span>
+          </h3>
+        </section>
+      )}
     </SiteLayout>
   );
 };
